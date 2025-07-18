2b83961853e4f706ff98207ba61c6b5fe89e17c2
codemarsh <91274691+codemarsh@users.noreply.github.com>
2025-07-18 13:33:16 -0400

    Update airgapped default values

--- network/variables.tf
+++ network/variables.tf
@@ -74,6 +74,7 @@ variable "airgapped"  {
   description = "test  variable for airgapped instead of separate vars"
   default     =  {
          enabled   = false
+         ocp_ver_rel = ""
          mirror_ip   = ""
          mirror_fqdn = ""
          mirror_port = ""
