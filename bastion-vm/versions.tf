c697eeee3eac13bce076314642f99ae29e8df36a
codemarsh <91274691+codemarsh@users.noreply.github.com>
2025-07-21 12:03:50 -0400

    Set ignition provider for config spec 3.4.0

--- bastion-vm/versions.tf
+++ bastion-vm/versions.tf
@@ -15,7 +15,7 @@ terraform {
   }
     ignition = {
       source = "community-terraform-providers/ignition"
-      // version = "2.1.6"
+      version = "2.4.1"
     }
   }
   required_version = ">= 0.13"
